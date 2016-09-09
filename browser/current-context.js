c96e37c99df741a6b1e246e9d9d79707b0662576
josieusa <emilianod182@gmail.com>
2016-09-09 14:24:53 +0200

    Merge upstream/master into master to sync with upstream repo Fix comment

--- browser/current-context.js
+++ browser/current-context.js
@@ -1,5 +1,5 @@
 // Copyright IBM Corp. 2015. All Rights Reserved.
-// Node module: loopback-context-cls
+// Node module: loopback-context
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
