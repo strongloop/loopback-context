0c76b5570e7fa3e7a491ab00e70086f98d9f56cc
josieusa <emilianod182@gmail.com>
2016-09-09 14:47:45 +0200

    Merge upstream/master into feature/cls-hooked-2 to sync with upstream repo

--- server/middleware/per-request.js
+++ server/middleware/per-request.js
@@ -1,5 +1,5 @@
 // Copyright IBM Corp. 2014,2016. All Rights Reserved.
-// Node module: loopback-context
+// Node module: loopback-context-cls
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
