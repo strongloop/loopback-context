391d9eb2adbf81e3909391f138ba2d8f1436d879
Agnes Lin <agneslin.lin@ibm.com>
2019-05-07 14:03:34 -0400

    chore: update copyrights years

--- test/stub-modules/when-3.7.7/index.js
+++ test/stub-modules/when-3.7.7/index.js
@@ -1,2 +1,7 @@
+// Copyright IBM Corp. 2017. All Rights Reserved.
+// Node module: loopback-context
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 'use strict';
 module.exports = require('when');
